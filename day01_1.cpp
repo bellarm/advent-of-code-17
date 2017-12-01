429c8e450a6ed3512ff6654a41209f9c506c2a17
Bella Mangunsong <rachelitabella@rocketmail.com>
2017-12-01 16:35:01 +1100

    changed a bit

--- day01_1.cpp
+++ day01_1.cpp
@@ -6,14 +6,14 @@ int main(void) {
   std::cin >> s;
   int sum = 0;
   char prev = s[0];
-  if (prev == s[s.size()-1])
-    sum += prev - '0';
   for (unsigned i = 1; i < s.size(); ++i) {
     char cur = s[i];
     if (prev == cur)
       sum += (cur - '0');
     prev = cur;
   }
+  if (prev == s[0])
+    sum += prev - '0';
   std::cout << sum << "\n";
   return 0;
 }
