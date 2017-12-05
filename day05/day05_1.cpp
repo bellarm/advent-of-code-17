daa3b5fff2d888256b607fc2651eb349ffd2bf33
Bella Mangunsong <rachelitabella@rocketmail.com>
2017-12-05 23:19:44 +1100

    part1 of day5

---
+++ day05/day05_1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <iterator>
+
+int main(void) {
+  std::vector<int> instr{std::istream_iterator<int>(std::cin), {}};
+  int i = 0;
+  int step = 0;
+  int size = instr.size();
+  while (true) {
+    if (i < 0 || i >= size)
+      break;
+    i += instr[i]++;
+    step++;
+  }
+  std::cout << step << "\n";
+  return 0;
+}
+
