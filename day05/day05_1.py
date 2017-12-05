2770a1e453c980104a60a7c8ff1fcbb8b62e1f02
Bella Mangunsong <rachelitabella@rocketmail.com>
2017-12-05 22:44:35 +1100

    part1 of day5

--- day05/day05_1.py
+++ day05/day05_1.py
@@ -1,17 +1,16 @@
 import sys
 
 instr = [int(line) for line in sys.stdin]
-
 step = 0
 i = 0
 size = len(instr)
+
 while True:
     if i >= size:
         break
     elif i >= 0:
-        new_i = i + instr[i]
         instr[i] += 1
-        i = new_i
+        i += instr[i] - 1
     else:
         i += instr[i]
     step += 1
